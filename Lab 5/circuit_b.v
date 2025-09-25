e2199fb07973fc62a131d03db41888ae7ef3e165
Alec Bozzo <160996959+AlecJBPro@users.noreply.github.com>
2025-09-25 14:22:18 -0600

    Add files via upload

---
+++ Lab 5/circuit_b.v
@@ -0,0 +1,9 @@
+module circuit_b(
+    input A, B, C, D,
+    // Declare Y output
+    output Y
+);
+
+    // Enter logic equation here
+    assign Y = (~C & ~D) | (A & B) | (B & C & ~D);
+endmodule
