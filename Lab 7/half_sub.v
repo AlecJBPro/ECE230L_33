7eecd90acf984207ff51a0a831c873b92e977a2c
Alec Bozzo <160996959+AlecJBPro@users.noreply.github.com>
2025-10-09 16:13:08 -0600

    Delete Lab 7 directory

--- Lab 7/half_sub.v
+++
@@ -1,9 +0,0 @@
-    module half_sub(
-        input A, B,
-        output Y, Borrow
-        );
-        
-        assign Y = (~A & B) | (A & ~B);
-        assign Borrow = ~A & B;
-        
-endmodule
